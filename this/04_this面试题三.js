b842ce84858df895341ba6e473fa209b01f34382
xionglp <785874752@qq.com>
2025-01-17 23:07:06 +0800

    代码执行顺序事件循环-this指向问题

---
+++ this/04_this面试题三.js
@@ -0,0 +1,36 @@
+var name = 'window'
+
+function Person (name) {
+  this.name = name
+  this.foo1 = function () {
+    console.log(this.name)
+  },
+  this.foo2 = () => console.log(this.name),
+  this.foo3 = function () {
+    return function () {
+      console.log(this.name)
+    }
+  },
+  this.foo4 = function () {
+    return () => {
+      console.log(this.name)
+    }
+  }
+}
+
+var person1 = new Person('person1')
+var person2 = new Person('person2')
+
+person1.foo1() // 隐式绑定: person1
+person1.foo1.call(person2) // 显式绑定: person2
+
+person1.foo2() // 箭头函数不绑定this, 外层作用域是new绑定: person1
+person1.foo2.call(person2) // 箭头函数不绑定this, 外层作用域是new绑定: person1
+
+person1.foo3()() // 独立调用: window
+person1.foo3.call(person2)() // 独立调用: window
+person1.foo3().call(person2) // 显式绑定: person2
+
+person1.foo4()() // 箭头函数不绑定this, 外层作用域是隐式绑定: person1
+person1.foo4.call(person2)() // 箭头函数不绑定this, 外层作用域是显式绑定: person2
+person1.foo4().call(person2) // 箭头函数不绑定this, 外层作用域是隐式绑定: person1
